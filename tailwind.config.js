218b2792b929d31f05c60f5d94daec433636d12b
David Casanova <casanova.83130@gmail.com>
2024-06-08 20:45:05 +0200

    update conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,6 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
-      keyframes: {
-        scrolling: {
-          '0%': { transform: 'translateX(100%)' },
-          '100%': { transform: 'translateX(-100%)' },
-        },
-      },
-      animation: {
-        scrolling: 'scrolling 30s linear infinite',
-      },
     },
   },
   plugins: [],
